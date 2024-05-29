fdf152c91c60a826c467e06da3a883eab4434b0c
Sanish Maharjan <sanishmaharjan490@gmail.com>
2024-05-29 08:25:46 +0545

    changed settings

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -192,10 +192,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "user_authentication.UserAccount"
 
-SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=9),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
-}
 
 KHALTI_URL = config("KHALTI_URL")
 KHALTI_VERIFY_URL = config("KHALTI_VERIFY_URL")
